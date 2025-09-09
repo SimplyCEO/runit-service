dc2b2a318e3056166add01d20b0a34c279003add
SimplyCEO <simplyceo.developer@gmail.com>
2025-09-09 17:21:09 -0300

    Improve helper functions

--- src/helper.c
+++ src/helper.c
@@ -5,32 +5,33 @@
 void
 print_help(void)
 {
-  printf("%s: A custom tool to manage runit services.\n\
-          \r\n\
-          \rUsage: %s <mode> <service>\n\
-          \r\n\
-          \rAvailable modes:\n\
-          \r\tlist    -  List the services located at '/etc/runit/sv'.\n\
-          \r\tunlink  -  Unlink the service from '/etc/runit/runsvdir/default'.\n\
-          \r\tlink    -  Link the service from '/etc/runit/sv' to '/etc/runit/runsvdir/default'.\n\
-          \r\tenabled -  List the enabled services located at '/etc/runit/runsvdir/default'.\n\
-          \r\tdisable -  Disable a service from activating at boot.\n\
-          \r\tenable  -  Enable a service activation after boot.\n\
-          \r\tstatus  -  Display the service information.\n\
-          \r\tstart   -  Start the service.\n\
-          \r\trestart -  Stop and start the service.\n\
-          \r\tstop    -  Stop the service.\n\
-          \r\tpurge   -  Will stop, unlink, and remove the service permanently.\n\
-          \r\tjournal -  Listen kernel logs.\n\
-          \r\0", appname, appname);
+  printf("%s%s\n\n%s%s%s\n\n%s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n",
+    appname, ": A custom tool to manage runit services.",
+    "Usage: ", appname, " <mode> <service>",
+    "Available modes:",
+    "list    -  List all available services.",
+    "unlink  -  Detach the service from directory.",
+    "link    -  Create a symbolic link of the service to the `runsvdir` directory.",
+    "enabled -  List the enabled services.",
+    "disable -  Disable a service from activating at boot.",
+    "enable  -  Enable a service activation after boot.",
+    "status  -  Display the service information.",
+    "start   -  Start the service.",
+    "restart -  Stop and start the service.",
+    "stop    -  Stop the service.",
+    "purge   -  Will stop, unlink, and remove the service permanently.",
+    "journal -  Listen kernel logs."
+  );
 }
 
 void
 print_version(void)
 {
-  printf("%s a0.2.1:\n\
-          \r\tGitHub: https://github.com/SimplyCEO/runit-service.git\n\
-          \r\tGitLab: https://gitlab.com/SimplyCEO/runit-service.git\n", appname);
+  printf("%s %s%d.%d.%d:\n  %s\n  %s\n",
+    appname, VERSION_PREFIX, VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH,
+    "GitHub: https://github.com/SimplyCEO/runit-service.git",
+    "GitLab: https://gitlab.com/SimplyCEO/runit-service.git"
+  );
 }
 
 unsigned char
--- src/main.h
+++ src/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
 
+#define VERSION_PREFIX "a"
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 2
+#define VERSION_PATCH 1
+
 #define RUNIT_AVAILABLE_SERVICES "/etc/runit/sv"
 #define RUNIT_LOADED_SERVICE_PATH "/run/runit/service"
 #define RUNIT_DEFAULT_SERVICE_PATH "/etc/runit/runsvdir/default"
