78a824b894704bccf28f575d200b416365e72313
SimplyCEO <simplyceo.developer@gmail.com>
2025-09-10 01:27:09 -0300

    Push version `a0.2.2`
    Refactored most functions (d0de6f4c3e9bf05763237a7b20f6021dba094b84)
    Remove unused argument parser function (3d617940e83808c72749f2a0f6b9dfaf148e5058)
    Fixed `appname` being displayed with full path (9a97c37c58ad96553fec0e89506647dce51c4507)
    Improve helper functions (dc2b2a318e3056166add01d20b0a34c279003add)

--- src/main.h
+++ src/main.h
@@ -6,7 +6,7 @@
 #define VERSION_PREFIX "a"
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 2
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 #define RUNIT_AVAILABLE_SERVICES "/etc/runit/sv"
 #define RUNIT_LOADED_SERVICE_PATH "/run/runit/service"
